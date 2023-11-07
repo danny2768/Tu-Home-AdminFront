8005e76faa3cc7749394228f42493846537d1b26
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2023-11-07 06:59:49 -0500

    7/11/2023: Added env variables

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,5 @@
+
+
+export const environments = {
+  baseUrl: 'https://tu-home-backend.up.railway.app/'
+}
---
+++ src/environments/environments.ts
@@ -0,0 +1,8 @@
+
+// export const environments = {
+//   baseUrl: 'https://tu-home-backend.up.railway.app/'
+// }
+
+export const environments = {
+  baseUrl: 'http://localhost:8080'
+}
