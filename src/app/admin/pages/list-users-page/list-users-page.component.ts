0a546a7e8496dff76affba4bb49b46c133f768be
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2023-11-13 14:36:32 -0500

    13/11/2023: onDelete method added

--- src/app/admin/pages/list-users-page/list-users-page.component.ts
+++ src/app/admin/pages/list-users-page/list-users-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AdminService } from '../../services/admin.service';
 import { User } from 'src/app/shared/interfaces/user.interface';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'admin-list-users-page',
@@ -11,10 +12,15 @@ import { Subscription } from 'rxjs';
 export class ListUsersPageComponent implements OnInit, OnDestroy{
 
   private subscription?: Subscription;
+  private subscription1?: Subscription;
+
   public userList?: User[];
 
 
-  constructor( private adminService: AdminService ) { }
+  constructor(
+    private adminService: AdminService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
     // * Obtener lista de usuarios en la pagina
@@ -28,6 +34,25 @@ export class ListUsersPageComponent implements OnInit, OnDestroy{
     if ( this.subscription ) {
       this.subscription.unsubscribe();
     }
+  }
+
+  onDelete( id: string): boolean {
+
+    const resp = confirm(`Estas seguro de que deseas eliminar al usuario ${id}`)
+
+    if( resp ) {
+      this.subscription1 = this.adminService.deleteUserById( id )
+      .subscribe( wasDeleted => {
+        if ( !wasDeleted ) {
+          return false;
+        }
+
+        alert('Usuario eliminado correctamente');
+        window.location.reload();
+        return true;
+      })
+    }
+    return false;
   }
 
 }
