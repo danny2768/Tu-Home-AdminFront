1c31cd94a8a49528c68b1549ca0408711e6424bf
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2023-12-06 11:40:55 -0500

    06/12/2023: Build config

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,3 @@
-
-
 export const environments = {
   baseUrl: 'https://tu-home-backend.up.railway.app'
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,3 @@
-// https://tu-home-backend.up.railway.app/swagger-ui/index.html#/
 export const environments = {
   baseUrl: 'https://tu-home-backend.up.railway.app'
 }
